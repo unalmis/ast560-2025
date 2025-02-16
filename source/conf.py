4a4b65ab56a737514027c7ae5e270a0efe282bf8
ammarhakim <a.hakim777@gmail.com>
2025-02-16 13:22:55 -0500

    More

--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.autosectionlabel',
-    'sphinx_immaterial',
+#    'sphinx_immaterial',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -90,8 +90,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinxdoc'
-html_theme = "sphinx_immaterial"
+html_theme = 'sphinxdoc'
+#html_theme = "sphinx_immaterial"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
