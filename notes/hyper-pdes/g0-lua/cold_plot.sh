8ca8824b17886c2c6a6ec4f36422bf91d9e537e4
ammarhakim <a.hakim777@gmail.com>
2025-03-20 07:54:38 -0400

    More work on the FV schemes for hyperbolic PDEs

---
+++ notes/hyper-pdes/g0-lua/cold_plot.sh
@@ -0,0 +1,3 @@
+pgkyl coldfluid_clouda-cold_0.gkyl sel -c0 pl --saveas "cold-fluid-0.png" --no-show --title="Density \$t=0\$"
+pgkyl coldfluid_clouda-cold_1.gkyl sel -c0 pl --saveas "cold-fluid-1.png" --no-show --title="Density \$t=1\$"
+pgkyl coldfluid_clouda-cold_2.gkyl sel -c0 pl --saveas "cold-fluid-2.png" --no-show --title="Density \$t=2\$"
